7eeab2817c6a8ce7306b895c8d186ad5c050dce8
jeongsangtae <wjdtkdxo55@naver.com>
2023-11-29 06:30:38 +0900

    React Complete Training Typescript - Working with refs and useRef (App.tsx, NewTodo.tsx edit)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
       <Todos items={todos} />
+      <NewTodo />
     </div>
   );
 }
