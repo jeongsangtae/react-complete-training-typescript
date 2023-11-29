d198c0176ced34b5b093a0e8c71f5192f827dde8
jeongsangtae <wjdtkdxo55@naver.com>
2023-11-30 07:31:12 +0900

    React Complete Training Typescript - Practice: Removing Todos (App.tsx, Todos.tsx, TodoItem.tsx edit)

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -4,12 +4,19 @@ import Todo from "../models/todo";
 import TodoItem from "./TodoItem";
 import classes from "./Todos.module.css";
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{
+  items: Todo[];
+  onRemoveTodo: (todoId: string) => void;
+}> = (props) => {
   return (
     <ul className={classes.todos}>
       {props.items.map((item) => (
         // <li key={item.id}>{item.text}</li>
-        <TodoItem key={item.id} text={item.text} />
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
